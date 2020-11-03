b589ada3547664b82347b2f956448d64d9b9b2b1
Jeff <jeffyjefflabs@users.noreply.github.com>
2020-11-03 16:10:41 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nbgallery',
+    version='0.0.1',
+    description='Python interface to local nbgallery',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nbgallery/nbgallery-python',
+    author='https://github.com/nbgallery',
+    license='MIT',
+    packages=['nbgallery'],
+    install_requires=[
+        'appdirs',
+        'inflect',
+        'mysqlclient',
+        'pandas',
+        'ruamel.yaml',
+        'SQLAlchemy',
+        'sqlalchemy-mixins',
+        'SQLAlchemy-Utils'
+    ]
+)
